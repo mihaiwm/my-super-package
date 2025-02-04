4fc150f63d15a73ebdd0fc3c999e1b7604209d31
Mihai H <mihai.h@wavemyth.com>
2025-02-04 16:44:01 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.displayValue = displayValue;
 function helloWorldFunc(value) {
     console.log("Hello world test ".concat(value));
+}
+module.exports = helloWorldFunc;
+function displayValue(value) {
+    console.log("".concat(value));
 }
 module.exports = helloWorldFunc;
